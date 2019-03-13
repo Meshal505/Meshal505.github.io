457b15561deae149490a6dc66996aa16d77e61d1
Meshal505 <rings-3@hotmail.com>
2019-03-13 19:00:53 +0300

    read me file and the whole game is ready

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Meshal505.github.io
+
+
+the technologies that i used to make this game are JS ,jQuery,CSS and HTML. 
+
+i built the landing page using HTML and CSS only.
+
+My approach to make this is as listed:-
+
+1- i first built the structure of page using HTML and CSS when i built everything perfectly as i want it.
+
+2- i jumped into JS and jQuery to start the real work.
+
+3- i first tried to know what are the tools that i will need like saying i need arrays to store values of each player i need functions to count the plays and i need function to restart the game and i need a function to start the game over. i need a method to push the scores and to make them displayed on the screen. 
+
+4- i used arrays ,if statements a lot i used an anonymous function and i used named functions. 
--- main.js
+++ main.js
@@ -4,146 +4,135 @@ let player2 = [];
 console.log(player1)
 var player1Score = 0;
 var player2Score = 0;
-//this to know who is player is playing.
-let current_player = 0 ;
-//o x
+//this to know who is player is playing adn to set the first player to play 
+let current_player = 0;
+//this to stop function checkwhoWins function when there is winner so its used as a light switch.
 var whoWins = false;
-// let win = ""
+// winning comb to compare players moves with them.
 let winCombo = [
-        [1,2,3],
-        [4,5,6], 
-        [7,8,9],
-    
-        [1,5,9],
-        [3,5,7],
-        [1,4,7],
-        [2,5,8],
-        [3,6,9]
-                ];
-             
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
 
-// console.log(winCombo);
-// function startNewRound (){
+    [1, 5, 9],
+    [3, 5, 7],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9]
+];
 
-    
-     whoWins = false
-     player1 = [];
-     player2 = [];
-     console.log(player1)
-    //  console.log(player2)
-    
-
-// }
-function resetGame (){
+// this to rest the game when there is a winner or when i click on the button 
+//its named function 
+function resetGame() {
     $(".square").attr("style", "undefined")
     $(".square").removeAttr("style");
+    whoWins = false
+    player1 = [];
+    player2 = [];
+    current_player = 0;
+    $("#dev").html("If you want to play again you must open the dev tools.");
 
-    // $(".square").each( function(){
-    //         $(this).removeAttr("style");
-    //         console.log(this)
-        whoWins = false
-        player1 =[];
-        player2 =[];
-        current_player = 0;
-        $("#dev").html("If you want to play again you must open the dev tools.");
-
-    // });
 
 }
-$("#btn").on("click", function(){
-     resetGame ()
+// anonymous function so when the button is clicked the game will restart
+$("#btn").on("click", function () {
+    resetGame()
 })
 
+//this is named function to changePlayers
+function changePlayer() {
 
-function changePlayer(){
-
-   if( current_player == 0){
-    current_player = 1
-   }else{
-    current_player = 0
-   }
+    if (current_player == 0) {
+        current_player = 1
+    } else {
+        current_player = 0
+    }
 }
 
-
-$(".square").on("click", function(){ //when click on any zone1
+//this is to check if the square that the user will click on has style or not if it does not have style then it will apply style on it.
+$(".square").on("click", function () { //when click on any zone1
     console.log(player1.length)
-    
+
     let id = $(this).attr("id") //get the attribute of id of particular square clicked and store in variable id
-            if(current_player == 0){
-                            if (whoWins == false){
-                            if ($(this).attr("style") == undefined || $(this).attr("style") == "undefined"){ 
-                                $(this).css("background-image", "url('https://i.ytimg.com/vi/tRx9TCKJWSs/maxresdefault.jpg')").css("background-size","100px 100px")
-                              player1.push(parseInt(id));
-                              console.log(id);
-                              
-                             
-                             changePlayer() } }
-                             if (player1.length == 4) { 
-                                console.log("hi")
-                              resetGame();
-                          }
+    if (current_player == 0) {
+        if (whoWins == false) {
+            if ($(this).attr("style") == undefined || $(this).attr("style") == "undefined") {
+                $(this).css("background-image", "url('https://i.ytimg.com/vi/tRx9TCKJWSs/maxresdefault.jpg')").css("background-size", "100px 100px")
+                player1.push(parseInt(id));
+                console.log(id);
+
+
+                changePlayer()
+            }
+        }
+        if (player1.length == 4) {
+            console.log("hi")
+            resetGame();
+        }
 
     }
-    else if (current_player ==1){
-                         if (whoWins == false) {
-                         if ($(this).attr("style") == undefined || $(this).attr("style") == "undefined") {
-                              $(this).css("background-image", "url('http://pluspng.com/img-png/circle-png-circle-png-hd-1600.png')").css("background-size","100px 100px")
-                        player2.push(parseInt(id));
-        
-                                changePlayer() } } }
-                             
-    
-    if (whoWins !== true){
-        checkWhoWins(player1,player2)
-        
+    else if (current_player == 1) {
+        if (whoWins == false) {
+            if ($(this).attr("style") == undefined || $(this).attr("style") == "undefined") {
+                $(this).css("background-image", "url('http://pluspng.com/img-png/circle-png-circle-png-hd-1600.png')").css("background-size", "100px 100px")
+                player2.push(parseInt(id));
+
+                changePlayer()
+            }
+        }
     }
-    });
 
-function checkWhoWins (player1,player2){ 
-                for ( var i = 0; i < winCombo.length ; i++ ){
-                       
-                        
-                        if (player1.sort().join('') == winCombo[i].join('')){
-                            
-                            whoWins = true;
-                            if (whoWins == true) {
-                                player1Score++
-                               $("#player1").html(player1Score) 
-                               
-                                
-                                console.log(player1Score);}
-                                swal({
-                                    title: "EXCELLENT X WON !",
-                                    icon: "success",
-                                  });
-                                  resetGame ();
-                                 
-                                    
-                                    
-                        
-                            }
-                            
-                        
+// here the function checkwhoWins will only run if the value of who wins is not true once its true then it will stop running.
+    if (whoWins !== true) {
+        checkWhoWins(player1, player2)
 
-                           else if (player2.sort().join('') == winCombo[i].join('')) {
-                            whoWins = true;
-                            if(whoWins == true) {
-                                player2Score++
-                                $('#player2').html(player2Score)
-                                console.log(player2Score);
-                                console.log("Player 2 won ")
-                                swal({
-                                    title: "EXCELLENT O  WON !",
-                                    icon: "success",
-                                  });
-                                
-                                  resetGame ();
-                            
-                            }
-                            // document.write("player 2 won");
-                        }
-                        
-                }
+    }
+});
+
+function checkWhoWins(player1, player2) {
+    for (var i = 0; i < winCombo.length; i++) {
+
+
+        if (player1.sort().join('') == winCombo[i].join('')) {
+
+            whoWins = true;
+            if (whoWins == true) {
+                player1Score++
+                $("#player1").html(player1Score)
+
+
+                console.log(player1Score);
+            }
+            swal({
+                title: "EXCELLENT X WON !",
+                icon: "success",
+            });
+            resetGame();
+
+
+
+
+        }
+
+
+// here we sort to organize and join to make them as string 
+        else if (player2.sort().join('') == winCombo[i].join('')) {
+            whoWins = true;
+            if (whoWins == true) {
+                player2Score++
+                $('#player2').html(player2Score)
+                console.log(player2Score);
+                console.log("Player 2 won ")
+                swal({
+                    title: "EXCELLENT O  WON !",
+                    icon: "success",
+                });
+
+                resetGame();
+
             }
             
-            // }
+        }
+
+    }
+}
